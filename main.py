a7fc1f519ef28fef485bb07007fe01229e45ad6f
Ethan Woolley <adamawoolley@gmail.com>
2019-11-11 20:44:03 -0800

    Switching computers

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from vpython import *
-
-global G
-G = 6.673e-11
+from time import sleep
 
 class system():
-    def __init__(self, *args, deltat=0.005, rate=100):
+    def __init__(self, deltat=0.005, rate=100, *args):
         self.bodies = args
         self.deltat = deltat
         self.rate = rate
@@ -13,40 +11,43 @@ class system():
         while True:
             for body in self.bodies:
                 body.update_position(self.bodies, self.deltat)
-            rate(self.rate)
-
-class celectial_object():
-    def __init__(self, pos=vector(0,0,0), radius=10, color=color.orange, velocity=vector(0,0,0), mass=10):
+            sleep(self.rate)
+            
+class celectialObject():
+    def __init__(self, pos=vector(0,0,0), radius=10, color=color.orange, velocity=vector(0,0,0), mass=10, name="george"):
         self.sphere = sphere(pos=pos, radius=radius, color=color)
         self.velocity = velocity
         self.mass = mass
+        self.name = name
 
-    def distanceto(self, pos, other):
-        return sqrt((pos.x - other.x)**2 + (pos.y - other.y)**2 + (pos.z - other.z)**2)
+    def distance_to(self, other):
+        return sqrt((self.sphere.pos.x - other.sphere.pos.x)**2 \
+                  + (self.sphere.pos.y - other.sphere.pos.y)**2 \
+                  + (self.sphere.pos.z - other.sphere.pos.z)**2)
 
-    def calculate_unit_vector(self, pos, other):
-        return (pos - other)/self.distanceto(pos, other)
+    def unit_vector_to(self, other):
+        return (other.sphere.pos - self.sphere.pos)/self.distance_to(other)
 
-    def calculate_gravity(self, pos, other):
-        distance = self.distanceto(pos, other.sphere.pos)
-        unit_vector = self.calculate_unit_vector(pos, other.sphere.pos)
-        return unit_vector * (G*((self.mass*other.mass)/distance**2))
+    def calculate_gravity(self, other):
+        print(self.name, "to", other.name, self.unit_vector_to(other))
+        return self.unit_vector_to(other) * (6.673e-11*((self.mass*other.mass)/self.distance_to(other)**2))
 
     def update_position(self, bodies, deltat):
-        future = self.sphere.pos + self.velocity * deltat
         acceleration = vector(0,0,0)
         for body in bodies:
             if body != self:
-                current_acceleration = self.calculate_gravity(self.sphere.pos, body)
-                future_acceleration = self.calculate_gravity(future, body)
-                acceleration += (current_acceleration + future_acceleration)/2
-        self.velocity += acceleration
+                acceleration += self.calculate_gravity(body)
+        print(self.name, "acc", acceleration)
+        self.velocity += acceleration * deltat
+        print(self.name, "Pos", self.sphere.pos)
         self.sphere.pos += self.velocity * deltat
 
-sun = celectial_object(pos=vector(0,0,0), radius=16, color=color.orange, velocity=vector(0,0,0), mass=20)
+if __name__ == '__main__':
 
-planet = celectial_object(pos=vector(-100,0,0), radius=4, color=color.green, velocity=vector(0,0,0), mass=5)
+    sun = celectialObject(pos=vector(0,0,0), radius=695510, color=color.orange, velocity=vector(0,0,0), mass=1.9885e30, name='sun')
 
-s = system(sun, planet, deltat=0.005, rate=10)
+    mercury = celectialObject(pos=vector(57.9e6,0,0), radius=4879/2*100, color=color.green, velocity=vector(0,47.4,0), mass=0.33e24, name='mercury')
 
-s.run()
+    s = system(0.5, 5, sun, mercury)
+
+    s.run()
